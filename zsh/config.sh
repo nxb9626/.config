99c045d44a00846a529bf0c6648fa6ff7803a886
nxb9626 <nxb9626@rit.edu>
2023-10-22 15:52:19 -0400

    add way to track aliases in this repo, rather than in zshrc directly, as well as setup instruction

---
+++ zsh/config.sh
@@ -0,0 +1,7 @@
+
+# am lazy
+alias z="zellij"
+
+# keybinds
+alias tl="sed -i '' 's/catppuccin-mocha.yml/catppuccin-latte.yml/' ~/.config/alacritty/alacritty.yml"
+alias td="sed -i '' 's/catppuccin-latte.yml/catppuccin-mocha.yml/' ~/.config/alacritty/alacritty.yml"
