12e9786fb94ba45f0a46afee529c13667ea51f61
nxb9626 <nxb9626@rit.edu>
2024-01-14 23:52:25 -0500

    move todo 'app' to config.sh

--- zsh/config.sh
+++ zsh/config.sh
@@ -5,3 +5,8 @@ alias z="zellij"
 # keybinds
 alias tl="sed -i '' 's/catppuccin-mocha.yml/catppuccin-latte.yml/' ~/.config/alacritty/alacritty.yml"
 alias td="sed -i '' 's/catppuccin-latte.yml/catppuccin-mocha.yml/' ~/.config/alacritty/alacritty.yml"
+
+alias proj="cd ~/Projects"
+
+alias todo="rg -i todo | python3 -c \"from fileinput import input as i;[print(*[w[-1000:] for w in l.strip().split(':', 1)], sep=' | ') for l in i()]\" | nvim"
+
