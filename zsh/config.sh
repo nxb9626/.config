b231ad8c857c7f5f0e6f2ee2d860d70ecf5b6104
nxb9626 <nxb9626@rit.edu>
2024-11-11 14:40:02 -0500

    udpate to sync colors between terminal and zed thorugh sed;

--- zsh/config.sh
+++ zsh/config.sh
@@ -1,12 +1,15 @@
-
-# am lazy
-alias z="zellij"
+alias vim="nvim"
 
 # keybinds
-alias tl="sed -i '' 's/catppuccin-mocha.yml/catppuccin-latte.yml/' ~/.config/alacritty/alacritty.yml"
-alias td="sed -i '' 's/catppuccin-latte.yml/catppuccin-mocha.yml/' ~/.config/alacritty/alacritty.yml"
+alias tl="sed -i '' 's/catppuccin-mocha.toml/catppuccin-latte.toml/' ~/.config/alacritty/alacritty.toml;\
+					sed -i '' 's/Catppuccin Mocha/Catppuccin Latte/' ~/.config/zed/settings.json;\
+"
+alias td="sed -i '' 's/catppuccin-latte.toml/catppuccin-mocha.toml/' ~/.config/alacritty/alacritty.toml;\
+					sed -i '' 's/Catppuccin Latte/Catppuccin Mocha/' ~/.config/zed/settings.json;\
+"
 
 alias proj="cd ~/Projects"
 
 alias todo="rg -i todo | python3 -c \"from fileinput import input as i;[print(*[w[-1000:].strip() for w in l.strip().split(':', 1)], sep=' | ') for l in i()]\" | nvim "
-alias minecraft ="open /Applications/Minecraft.app/Contents/MacOS/launcher"
+
+alias minecraft="open /Applications/Minecraft.app/Contents/MacOS/launcher" # launching launcher through terminal allows for voice mod to work
