6473c9f8d67b22529b2dd62e46622a8b506046ab
nxb9626 <nxb9626@rit.edu>
2025-08-03 20:42:12 -0400

    update some settings

--- zsh/config.sh
+++ zsh/config.sh
@@ -10,12 +10,15 @@ alias vim="nvim"
 
 # keybinds
 alias tl="sed -i '' 's/catppuccin-mocha.toml/catppuccin-latte.toml/' ~/.config/alacritty/alacritty.toml;\
-					sed -i '' 's/Catppuccin Mocha/Catppuccin Latte/' ~/.config/zed/settings.json;\
-"
-alias td="sed -i '' 's/catppuccin-latte.toml/catppuccin-mocha.toml/' ~/.config/alacritty/alacritty.toml;\
-					sed -i '' 's/Catppuccin Latte/Catppuccin Mocha/' ~/.config/zed/settings.json;\
+					osascript -e 'tell application \"System Events\" to tell appearance preferences to set dark mode to not dark mode'
 "
 
+# sed -i '' 's/Catppuccin Mocha/Catppuccin Latte/' ~/.config/zed/settings.json;\
+alias td="sed -i '' 's/catppuccin-latte.toml/catppuccin-mocha.toml/' ~/.config/alacritty/alacritty.toml;\
+					osascript -e 'tell application \"System Events\" to tell appearance preferences to set dark mode to not dark mode'
+"
+
+# sed -i '' 's/Catppuccin Latte/Catppuccin Mocha/' ~/.config/zed/settings.json;\
 alias proj="cd ~/Projects"
 
 alias todo="rg -i todo | python3 -c \"from fileinput import input as i;[print(*[w[-1000:].strip() for w in l.strip().split(':', 1)], sep=' | ') for l in i()]\" | nvim "
